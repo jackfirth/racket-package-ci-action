267940d6c88ec1e039552caeec0cf483b038a4ea
Jack Firth <jackhfirth@gmail.com>
2019-08-28 05:49:10 -0400

    Clarify context of workaround

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,10 @@ cd "$GITHUB_WORKSPACE"
 # separate actions for each of those steps.
 if [ "$INPUT_DIRECTORY" -eq "." ]
 then
-  # installing a linked package with source directory "." raises an error for
-  # some bizarre reason, even when the package's name is specified explicitly
-  # with --name. This is especially strange because source directories like
-  # "./foo" work just fine.
+  # As of August 2019, installing a linked package with source directory "."
+  # raises an error for some bizarre reason, even when the package's name is
+  # specified explicitly with --name. This is especially strange because source
+  # directories like "./foo" work just fine.
   raco pkg install --name "$INPUT_NAME" --batch --auto --link
 else
   raco pkg install --name "$INPUT_NAME" --batch --auto --link "$INPUT_DIRECTORY"
